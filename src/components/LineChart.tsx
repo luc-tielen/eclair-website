c162ca2e1728378a5329f00a920f1bb7353e6d53
Luc Tielen <luc.tielen@gmail.com>
2023-04-16 21:26:06 +0200

    Add many visual improvements to perf charts

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -7,10 +7,12 @@ import {
   Title,
   CategoryScale,
   Tooltip,
+  Filler,
 } from "chart.js";
 import { useEffect, useState } from "react";
 
 interface LineChartProps {
+  title: string;
   data: {
     labels: string[];
     datasets: {
@@ -22,21 +24,49 @@ interface LineChartProps {
   };
 }
 
-const options = {
+const options = (title: string) => ({
   responsive: true,
   maintainAspectRatio: false,
+  // animation: false, TODO turn off
   interaction: {
     intersect: false,
-    mode: "index",
+    mode: "index" as const,
   },
-  plugins: {
-    tooltip: {
-      displayColors: false,
+  scales: {
+    y: {
+      title: {
+        display: true,
+        text: "Time (s)",
+        font: { size: 14 },
+      },
+      ticks: { padding: 12 },
     },
   },
-};
+  plugins: {
+    title: {
+      display: true,
+      text: title,
+      align: "start" as const,
+      font: { size: 16 },
+      padding: { bottom: 16 },
+    },
+    tooltip: {
+      displayColors: false,
+      callbacks: {
+        footer: (context: any) => {
+          const line = context[0];
+          const value = line.dataset.data[line.dataIndex];
+          const valueMs = value * 1000;
+          const roundedValue = Math.round(valueMs * 100) / 100;
+          return `${roundedValue} ms`;
+        },
+        label: () => "",
+      },
+    },
+  },
+});
 
-const LineChart = ({ data }: LineChartProps) => {
+const LineChart = ({ data, title }: LineChartProps) => {
   const [isRegistered, setIsRegistered] = useState(false);
 
   useEffect(() => {
@@ -46,7 +76,8 @@ const LineChart = ({ data }: LineChartProps) => {
       LinearScale,
       Title,
       CategoryScale,
-      Tooltip
+      Tooltip,
+      Filler
     );
     setIsRegistered(true);
   }, []);
@@ -55,7 +86,7 @@ const LineChart = ({ data }: LineChartProps) => {
 
   return (
     <div className="w-screen h-[200px] p-4">
-      <Line data={data} options={options} />
+      <Line data={data} options={options(title)} />
     </div>
   );
 };
