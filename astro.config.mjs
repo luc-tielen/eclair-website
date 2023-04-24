aeac2ed4c8ab32238d00467d3297f1e67685e9b8
Luc Tielen <luc.tielen@gmail.com>
2023-04-24 12:46:54 +0200

    Add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://eclair-lang.org",
-  integrations: [react(), tailwind(), mdx()],
+  integrations: [react(), tailwind(), mdx(), sitemap()],
   markdown: {
     syntaxHighlight: false, // should be handled by CodeBlock component
   },
