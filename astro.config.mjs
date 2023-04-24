76aaa8b937b1de1c0c0d93e0d0140b6aad52deec
Luc Tielen <luc.tielen@gmail.com>
2023-04-24 12:35:00 +0200

    Setup content collection for docs pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://eclair-lang.org",
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), mdx()],
+  markdown: {
+    syntaxHighlight: false, // should be handled by CodeBlock component
+  },
   vite: {
-    optimizeDeps: { exclude: ["tree-sitter-highlight"] },
+    optimizeDeps: {
+      exclude: ["tree-sitter-highlight"],
+    },
   },
 });
