a009cf5031fd41c427783fdd6e4438bbb4c1c592
Luc Tielen <luc.tielen@gmail.com>
2023-04-29 11:01:52 +0200

    Add table of contents component

---
+++ src/components/TableOfContents.tsx
@@ -0,0 +1,123 @@
+import { unescape } from "html-escaper";
+import { useEffect, useState } from "react";
+import type { TocItem } from "../util/generate-toc";
+import "./TableOfContents.css";
+
+// Same TableOfContents components as Astro docs, but with some modifications here and there.
+
+interface Props {
+  toc: TocItem[];
+  labels: {
+    onThisPage: string;
+  };
+}
+
+const toDepthClass = (depth: number) => {
+  switch (depth) {
+    case 3:
+      return "pl-2";
+    case 4:
+      return "pl-4";
+    case 5:
+      return "pl-6";
+    case 6:
+      return "pl-8";
+    default:
+      return "";
+  }
+};
+
+const TableOfContents = ({ toc, labels }: Props) => {
+  if (!toc[0]) return null;
+
+  const [currentHeading, setCurrentHeading] = useState({
+    slug: toc[0].slug,
+    text: toc[0].text,
+  });
+  const onThisPageID = "on-this-page-heading";
+
+  // TODO remove
+  useEffect(() => {
+    const setCurrent: IntersectionObserverCallback = (entries) => {
+      for (const entry of entries) {
+        if (entry.isIntersecting) {
+          const { id, textContent } = entry.target;
+          if (id === onThisPageID) continue;
+          setCurrentHeading({ slug: id, text: textContent || "" });
+          break;
+        }
+      }
+    };
+
+    const observerOptions: IntersectionObserverInit = {
+      // Negative top margin accounts for `scroll-margin`.
+      // Negative bottom margin means heading needs to be towards top of viewport to trigger intersection.
+      rootMargin: "-100px 0% -66%",
+      threshold: 1,
+    };
+
+    const headingsObserver = new IntersectionObserver(
+      setCurrent,
+      observerOptions
+    );
+
+    // Observe all the headings in the main page content.
+    document
+      .querySelectorAll("article :is(h1,h2,h3)")
+      .forEach((h) => headingsObserver.observe(h));
+
+    // Stop observing when the component is unmounted.
+    return () => headingsObserver.disconnect();
+  }, []);
+
+  const TableOfContentsItem = ({ heading }: { heading: TocItem }) => {
+    const { depth, slug, text, children } = heading;
+    return (
+      <li>
+        <a
+          className={`hover:text-slate-900 ${toDepthClass(depth)} ${currentHeading.slug === slug ? "current-header-link" : ""
+            }`.trim()}
+          href={`#${slug}`}
+        >
+          <svg
+            width="3"
+            height="24"
+            viewBox="0 -9 3 24"
+            className="inline-block mr-2 text-slate-400 overflow-visible group-hover:text-slate-600"
+          >
+            <path
+              d="M0 0L3 3L0 6"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.5"
+              stroke-linecap="round"
+            ></path>
+          </svg>
+          {unescape(text)}
+        </a>
+        {children.length > 0 ? (
+          <ul className="flex flex-col gap-1 text-slate-700">
+            {children.map((heading) => (
+              <TableOfContentsItem key={heading.slug} heading={heading} />
+            ))}
+          </ul>
+        ) : null}
+      </li>
+    );
+  };
+
+  return (
+    <>
+      <h2 className="font-medium pb-2" id={onThisPageID}>
+        {labels.onThisPage}
+      </h2>
+      <ul className="flex flex-col text-slate-700">
+        {toc.map((heading2) => (
+          <TableOfContentsItem key={heading2.slug} heading={heading2} />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default TableOfContents;
---
+++ src/content/docs/grounding.md
@@ -0,0 +1,38 @@
+---
+title: Grounding
+---
+
+Unlike in most other languages, Eclair doesn't have explicit initialization of
+variables. Variables just "appear" in the body of a rule and can be used
+immediately. Even when you are "assigning" a value to a variable, you are
+actually telling the compiler to unify both sides of an equation.
+
+In order to make sure that variables can always be properly initialized
+with values at runtime, Eclair has the concept of _grounding_. When a variable
+is grounded, it means that a variable can be allocated in memory, and is
+assigned a value to it before it is ever used.
+
+## Rules
+
+Below are all the grounding rules that Eclair checks against. These rules are
+checked indepedently for each rule or fact in a program. All variables must be
+grounded in all rules for a program to be semantically valid. If this is not the
+case, the compiler will abort with an error.
+
+1. No variables are allowed in top level facts (this is only allowed in Prolog);
+2. Literal values (numbers and strings) are always grounded;
+3. A binary operator (such as `+`) produces a grounded value if both left hand
+   side and right hand side of the operator are also grounded;
+4. Comparison and inequality operators (such as `<`) do not ground their arguments;
+5. A variable is grounded if it used as an argument in a rule clause (but this is
+   only the case when it is not used inside a negated rule clause);
+6. A variable is grounded if a literal is assigned to it;
+7. A variable is grounded if another grounded variable is assigned to it;
+8. All occurrences of a variable with the same name in a rule are grounded once
+   one of them is grounded;
+9. A [user-defined function or constraint](/user-defined-functions) does not
+   ground any of it's arguments. The return value of a user-defined function is
+   grounded if all of it's arguments are grounded.
+
+Note that these rules are recursive, meaning that the grounding property spreads
+throughout the body of a rule.
