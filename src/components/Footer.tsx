7bb0e8d595da391b114b343b8d65725f624d9ffc
Luc Tielen <luc.tielen@gmail.com>
2023-05-01 10:08:48 +0200

    Refactor NavBar and Footer components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => (
+  <footer className="pt-24 pb-2 text-slate-500">
+    <span className="flex items-center justify-center text-sm">
+      &copy; 2021-{new Date().getFullYear()} Luc Tielen
+    </span>
+  </footer>
+);
+
+export default Footer;
